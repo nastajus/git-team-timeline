9b780e8445c5e1176d45bd977c1486bee9d51d79
Ian Nastajus <ian.nastajus@gmail.com>
2016-07-22 23:35:13 -0400

    phpStorm idea files, empty file with wp comments

---
+++ git-team-timeline.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IAN
+ * Date: 22/07/2016
+ * Time: 11:17 PM
+ */
+
+/**
+ * Plugin Name: My first plugin
+ * Plugin URI: http://localhost
+ * Description this is a plugin for some tests
+ * Version: 1.0
+ * Author: Ian Nastajus
+ * Author URI: http://nastajus.com
+ * License: GPL2
+ */
+
+?>
